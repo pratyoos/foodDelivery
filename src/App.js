48569a856a67e34905882a3232f196fe525e73eb
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-04-19 08:44:47 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+// import Navbar from './components/Navbar';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import { CartProvider } from './components/ContextReducer';
+import MyOrder from './screens/MyOrder';
+
+
+function App() {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </a>
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+  
+  
+  
+  // return (
+  //   <CartProvider>
+  //     <Router>
+  //       <div>
+  //         <Routes>
+  //           <Route exact path="/" element={<Home />} />
+  //           <Route exact path="/login" element={<Login />} />
+  //           <Route exact path="/signup" element={<Signup />} />
+  //           <Route exact path="/myorder" element={<MyOrder />} />
+  //         </Routes>
+  //       </div>
+  //     </Router>
+  //   </CartProvider>
+  // );
+}
+
+export default App;
